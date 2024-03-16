074801dfcc5ff958c7d63c4ecd6dde6cf71dd320
Jackson Quintero <75138506+codigoarchivo@users.noreply.github.com>
2024-03-15 22:34:59 -0400

    add database

---
+++ src/databases/listVIdeos.ts
@@ -0,0 +1,10 @@
+import { IListVideo } from '@/interfaces';
+
+export const listVideo: IListVideo[] = [
+  { id: 1, link: '/', image: '/assets/Pic2.png', name: 'Lorem ipsum' },
+  { id: 2, link: '/', image: '/assets/Pic3.png', name: 'Lorem ipsum' },
+  { id: 3, link: '/', image: '/assets/Pic4.png', name: 'Lorem ipsum' },
+  { id: 3, link: '/', image: '/assets/Pic3.png', name: 'Lorem ipsum' },
+  { id: 3, link: '/', image: '/assets/Pic1.png', name: 'Lorem ipsum' },
+  { id: 3, link: '/', image: '/assets/Pic2.png', name: 'Lorem ipsum' },
+];
