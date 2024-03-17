daca874445b09a195c5a2b5cb4b50ab7d2a4397b
Jackson Quintero <75138506+codigoarchivo@users.noreply.github.com>
2024-03-16 22:04:59 -0400

    refactor Products

--- src/app/(nolatech)/ui/Products.tsx
+++ src/app/(nolatech)/ui/Products.tsx
@@ -6,8 +6,9 @@ export const Products = (props: IProducts) => {
   return (
     <div className='col-span-3 md:col-span-1'>
       <div className='flex flex-col mb-12 overflow-hidden cursor-pointer shadow-custom1'>
-        <Link href={props.link}>
-          <div className='flex-shrink-0'>
+        <Link href={props.link} title='link image'>
+          <div className='flex-shrink-0 relative'>
+            <div className='absolute z-[1] inset-0 bg-secondary/35 sm:from-sebg-secondary/95 sm:to-sebg-secondary/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l'></div>
             <Image
               src={props.image}
               className='w-full h-64'
@@ -21,9 +22,9 @@ export const Products = (props: IProducts) => {
           </div>
         </Link>
         <div className='flex flex-col justify-between flex-1 bg-primary p-5 text-center'>
-          <h3 className='text-2xl font-semibold leading-none tracking-tighter text-white'>
+          <h4 className='text-lg font-semibold leading-none tracking-tighter uppercase text-white'>
             {props.name}
-          </h3>
+          </h4>
         </div>
       </div>
     </div>
