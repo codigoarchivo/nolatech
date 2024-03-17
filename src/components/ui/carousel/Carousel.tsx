e146d8bd108b0d2945960b7ab9e800422ba6e7a7
Jackson Quintero <75138506+codigoarchivo@users.noreply.github.com>
2024-03-17 12:57:33 -0400

    refactor Carrousel

--- src/components/ui/carousel/Carousel.tsx
+++ src/components/ui/carousel/Carousel.tsx
@@ -35,7 +35,7 @@ export const Carrousel = ({ list }: CarouselProps) => {
     <div className='relative container mx-auto'>
       <button
         onClick={handlePrev}
-        className='absolute left-1 top-1/2 inline-block bg-primary rounded-full shadow-custom1 text-center text-sm font-semibold text-white hover:text-white outline-none transition duration-500 ease-in-out transform hover:bg-tertiary focus-visible:ring active:bg-secondary md:text-base'
+        className='absolute left z-10 top-1/2 inline-block bg-primary rounded-full shadow-custom1 text-center text-sm font-semibold text-white hover:text-white outline-none transition duration-500 ease-in-out transform hover:bg-tertiary focus-visible:ring active:bg-secondary md:text-base'
       >
         <svg
           xmlns='http://www.w3.org/2000/svg'
@@ -52,38 +52,44 @@ export const Carrousel = ({ list }: CarouselProps) => {
           />
         </svg>
       </button>
-      <div className='flex gap-6 p-5 justify-center overflow-x-hidden'>
-        <Image
-          src={list[currentIndex1]?.link}
-          alt={`Slide ${currentIndex1}`}
-          className='w-2/3 lg:w-1/4  h-60 lg:h-[450px] rounded-lg shadow-custom1 transition-opacity duration-300 ease-in-out'
-          width='0'
-          height='0'
-          sizes='100vw'
-          title={`Slide ${currentIndex1}`}
-        />
-        <Image
-          src={list[currentIndex2]?.link}
-          alt={`Slide ${currentIndex2}`}
-          className='w-2/3 lg:w-1/4  h-60 lg:h-[450px] rounded-lg shadow-custom1 transition-opacity duration-300 ease-in-out'
-          width='0'
-          height='0'
-          sizes='100vw'
-          title={`Slide ${currentIndex2}`}
-        />
-        <Image
-          src={list[currentIndex3]?.link}
-          alt={`Slide ${currentIndex3}`}
-          className='w-2/3 lg:w-1/4  h-60 lg:h-[450px] rounded-lg shadow-custom1 transition-opacity duration-300 ease-in-out'
-          width='0'
-          height='0'
-          sizes='100vw'
-          title={`Slide ${currentIndex3}`}
-        />
+      <div className='flex w-full gap-2 justify-center overflow-x-hidden'>
+        <div className='relative w-full lg:w-3/4  h-40 sm:h-[250px] md:h-[350px] lg:h-[450px]'>
+          <Image
+            src={list[currentIndex1]?.link}
+            alt={`Slide ${currentIndex1}`}
+            className='absolute w-full h-full  rounded-lg shadow-custom1 transition-opacity duration-300 ease-in-out'
+            width='0'
+            height='0'
+            sizes='100vw'
+            title={`Slide ${currentIndex1}`}
+          />
+        </div>
+        <div className='relative w-full lg:w-3/4  h-40 sm:h-[250px] md:h-[350px] lg:h-[450px]'>
+          <Image
+            src={list[currentIndex2]?.link}
+            alt={`Slide ${currentIndex2}`}
+            className='absolute w-full h-full rounded-lg shadow-custom1 transition-opacity duration-300 ease-in-out'
+            width='0'
+            height='0'
+            sizes='100vw'
+            title={`Slide ${currentIndex2}`}
+          />
+        </div>
+        <div className='relative w-full lg:w-3/4  h-40 sm:h-[250px] md:h-[350px] lg:h-[450px]'>
+          <Image
+            src={list[currentIndex3]?.link}
+            alt={`Slide ${currentIndex3}`}
+            className='absolute w-full h-full  rounded-lg shadow-custom1 transition-opacity duration-300 ease-in-out'
+            width='0'
+            height='0'
+            sizes='100vw' 
+            title={`Slide ${currentIndex3}`}
+          />
+        </div>
       </div>
       <button
         onClick={handleNext}
-        className='absolute right-1 top-1/2 inline-block bg-primary rounded-full shadow-custom1 text-center text-sm font-semibold text-white hover:text-white outline-none transition duration-500 ease-in-out transform hover:bg-tertiary focus-visible:ring active:bg-secondary md:text-base'
+        className='absolute right-1 top-1/2 z-10 inline-block bg-primary rounded-full shadow-custom1 text-center text-sm font-semibold text-white hover:text-white outline-none transition duration-500 ease-in-out transform hover:bg-tertiary focus-visible:ring active:bg-secondary md:text-base'
       >
         <svg
           xmlns='http://www.w3.org/2000/svg'
