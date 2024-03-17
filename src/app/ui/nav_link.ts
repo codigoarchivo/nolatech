8030dbe889c2efe7cb0a11416a71a16a49732c58
Jackson Quintero <75138506+codigoarchivo@users.noreply.github.com>
2024-03-16 22:08:21 -0400

    add navLink

---
+++ src/app/ui/nav_link.ts
@@ -0,0 +1,31 @@
+interface InavLink {
+  name: string;
+  link: string;
+}
+
+export const navLink: InavLink[] = [
+  {
+    name: 'LOREM IPSUM',
+    link: '/',
+  },
+  {
+    name: 'LOREM IPSUM',
+    link: '/',
+  },
+  {
+    name: 'LOREM IPSUM',
+    link: '/',
+  },
+  {
+    name: 'LOREM IPSUM',
+    link: '/',
+  },
+  {
+    name: 'LOREM IPSUM',
+    link: '/',
+  },
+  {
+    name: 'LOREM IPSUM',
+    link: '/',
+  },
+];
