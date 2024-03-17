23f2c74e7db407b4e0c2bace5729dd1df77066ab
Jackson Quintero <75138506+codigoarchivo@users.noreply.github.com>
2024-03-16 22:04:35 -0400

    refactor ListVideo

--- src/app/(nolatech)/ui/ListVideo.tsx
+++ src/app/(nolatech)/ui/ListVideo.tsx
@@ -3,7 +3,7 @@ import { ItemVideo } from './ItemVideo';
 export const ListVideo = () => {
   return (
     <div className='relative mx-auto max-w-7xl'>
-      <div className='grid max-w-lg gap-12 mx-auto mt-12 lg:grid-cols-3 lg:max-w-none'>
+      <div className='grid max-w-lg gap-0 lg:gap-12 mx-auto mt-12 lg:grid-cols-3 lg:max-w-none'>
         {listVideo.map((item, key) => (
           <ItemVideo {...item} key={key} />
         ))}
