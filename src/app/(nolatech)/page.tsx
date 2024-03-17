55549fb31b48c3c96594a07d1e54b175d8796cab
Jackson Quintero <75138506+codigoarchivo@users.noreply.github.com>
2024-03-16 22:02:03 -0400

    refactor Home

--- src/app/(nolatech)/page.tsx
+++ src/app/(nolatech)/page.tsx
@@ -1,51 +1,31 @@
 import Image from 'next/image';
 import Pic1 from '../../../public/assets/Pic1.png';
 import Pic5a from '../../../public/assets/Pic5a.png';
-import Pic6 from '../../../public/assets/Pic6.png';
-import JoinUs from '../../../public/assets/Join Us.png';
-import { Carrousel } from '@/components';
+import Pic6White from '../../../public/assets/Pic6White.png';
 import { SelectProducts } from './ui/SelectProducts';
 import { ListVideo } from './ui/ListVideo';
 import { Register } from './ui/Register';
+import { JoinUs } from './ui/JoinUs';
+import { CallMe } from './ui/CallMe';
+import { ListImage } from './ui/ListImage';
+import { Social } from './ui/Social';
 
 export default function Home() {
   return (
     <>
-      <ul className='social fixed z-50 top-1/3'>
-        <li>
-          <a className='fa fa-facebook' href='#'>
-            <span>Facebook</span>
-          </a>
-        </li>
-
-        <li>
-          <a className='fa fa-twitter' href='#'>
-            <span>Twitter</span>
-          </a>
-        </li>
-
-        <li>
-          <a className='fa fa-dribbble' href='#'>
-            <span>Dribbble</span>
-          </a>
-        </li>
-
-        <li>
-          <a className='fa fa-google-plus' href='#'>
-            <span>Google Plus</span>
-          </a>
-        </li>
-      </ul>{' '}
+      <div className='hidden md:block'>
+        <Social />
+      </div>
       <section>
-        <div className='mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16'>
+        <div className='container mx-auto px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16'>
           <div className='grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16'>
-            <div className='grid gap-2 md:gap-10'>
-              <h2 className='grid text-3xl font-bold sm:text-4xl  md:text-5xl lg:text-6xl'>
+            <div className='grid gap-2 md:gap-6'>
+              <h2 className='grid text-3xl font-bold sm:text-4xl md:text-5xl lg:text-6xl'>
                 <span>Lorem ipsum </span>{' '}
                 <span className='text-primary'>dolor sit amet</span>
               </h2>
 
-              <p className='mt-4 text-gray-500'>
+              <p className='mt-4 text-gray-500 indent-8'>
                 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
                 diam nonumy eirmod tempor invidunt ut labore et dolore magna
                 aliquyam erat, sed diam voluptua. At vero eos et accusam et
@@ -59,6 +39,7 @@ export default function Home() {
               </p>
             </div>
             <div className='relative h-96 overflow-hidden sm:h-80 lg:order-last lg:h-full shadow-custom1'>
+              <div className='absolute z-[1] inset-0 bg-secondary/35 sm:from-sebg-secondary/95 sm:to-sebg-secondary/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l'></div>
               <Image
                 src={Pic1}
                 className='absolute inset-0 h-full w-full object-cover'
@@ -74,13 +55,13 @@ export default function Home() {
         </div>
       </section>
       <section>
-        <div className='container mx-auto bg-white p-10 lg:p-20 shadow-custom1'>
+        <div className='container mx-auto p-10 lg:p-20 shadow-custom1 bg-slate-50'>
           {' '}
           <div className='grid gap-5 text-center mb-20 '>
             <h2 className='text-3xl font-bold sm:text-4xl md:text-5xl lg:text-6xl'>
               <span>Lorem</span> <span className='text-primary'>ipsum</span>
             </h2>
-            <p className='text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s'>
+            <p className='text-base leading-relaxed text-gray-500 xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s'>
               Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
               nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
               erat, sed diam voluptua.
@@ -91,18 +72,18 @@ export default function Home() {
           </div>
           <SelectProducts />
           <div className='w-full text-center'>
-            <button className='uppercase bg-transparent shadow-custom1 hover:bg-primary text-secondary font-semibold hover:text-white py-4 px-12 border border-primary transition duration-500 ease-in-out transform  hover:border-transparent rounded'>
-              LEARN MORE
+            <button className=' w-max inline-block rounded-md bg-transparent px-4 md:px-10 py-2 md:py-4 text-xs md:text-base shadow-custom1 text-center font-semibold border border-primary text-primary hover:text-white outline-none transition duration-500 ease-in-out transform hover:bg-tertiary focus-visible:ring active:bg-secondary'>
+              WHAT IS NEXT
             </button>
           </div>
         </div>
       </section>
-      <section className='bg-primary my-40'>
-        <div className='overflow-hidden lg:order-last lg:h-full'>
+      <section>
+        <div className='bg-primary overflow-hidden lg:order-last lg:h-full 2xl:container 2xl:mx-auto'>
           <div className='grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16'>
             <div className='col-span-1 md:col-span-1'>
-              <div className='grid min-h-min w-full md:w-2/3 py-20 px-5 sm:px-0 mx-auto justify-center'>
-                <h2 className='text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl grid gap-2 md:gap-5 text-left title-font mb-4 text-secondary'>
+              <div className='grid min-h-min w-full md:w-2/3 py-20 px-5 md:px-0 mx-auto justify-center'>
+                <h2 className='text-2xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl grid gap-2 md:gap-5 text-left title-font mb-4 text-secondary'>
                   <strong className='text-white'>
                     Lorem{' '}
                     <span className='text-secondary'>ipsum dolor sit</span>{' '}
@@ -110,13 +91,12 @@ export default function Home() {
                   </strong>{' '}
                 </h2>
                 <div className='grid gap-4 mt-16'>
-                  <button className='uppercase inline-block rounded-md bg-white px-8 py-6 shadow-custom1 text-center text-sm font-semibold text-secondary hover:text-white outline-none transition duration-500 ease-in-out transform hover:bg-secondary focus-visible:ring active:bg-secondary md:text-base'>
-                    Lorem ipsum
-                  </button>
-
-                  <button className='uppercase bg-transparent shadow-custom1 text-white font-semibold hover:text-secondary py-6 px-8 border border-white hover:border-secondary transition duration-500 ease-in-out transform  hover:border-transparent rounded'>
-                    Lorem ipsum
-                  </button>
+                  <button className='w-60 md:w-full inline-block rounded-md bg-white px-4 md:px-10 py-2 md:py-4 text-xs md:text-base shadow-custom1 text-center  font-semibold text-primary outline-none transition duration-500 ease-in-out transform hover:bg-secondary focus-visible:ring active:bg-secondary'>
+                    WHAT IS
+                  </button>{' '}
+                  <button className='w-60 md:w-full inline-block rounded-md bg-transparent px-4 md:px-10 py-2 md:py-4 text-xs md:text-base shadow-custom1 text-center font-semibold border border-white text-white hover:text-white outline-none transition duration-500 ease-in-out transform hover:bg-tertiary focus-visible:ring active:bg-secondary'>
+                    WHAT IS NEXT
+                  </button>{' '}
                 </div>
               </div>
             </div>
@@ -137,7 +117,7 @@ export default function Home() {
         </div>
       </section>
       <section>
-        <div className='container mx-auto bg-white p-10 lg:p-20 shadow-custom1'>
+        <div className='container mx-auto bg-slate-50 p-10 lg:p-20 shadow-custom1'>
           <div className='text-center mb-20'>
             <h2 className='sm:text-7xl text-2xl font-bold title-font text-gray-900 mb-4'>
               <span>Latest</span> <span className='text-primary'>videos</span>
@@ -146,33 +126,24 @@ export default function Home() {
 
           <ListVideo />
           <div className='w-full text-center'>
-            <button className='uppercase  w-max inline-block rounded-md bg-primary px-8 py-3 shadow-custom1 text-center text-sm font-semibold text-white outline-none transition duration-500 ease-in-out transform hover:bg-tertiary focus-visible:ring active:bg-secondary md:text-base'>
+            <button className=' w-max inline-block rounded-md bg-primary px-4 md:px-10 py-2 md:py-4 text-xs md:text-base shadow-custom1 text-center  font-semibold text-white outline-none transition duration-500 ease-in-out transform hover:bg-tertiary focus-visible:ring active:bg-secondary'>
               WHAT IS NEXT
             </button>
           </div>
         </div>
       </section>
       <section>
-        <div className='relative'>
-          <Image
-            src={JoinUs}
-            className='w-full h-full object-fill absolute z-[-1]'
-            alt={'prb_header'}
-            title={'Prb_Header'}
-            width='0'
-            height='0'
-            quality={100}
-            sizes='100vw'
-          />
-
+        <div className='relative 2xl:container 2xl:mx-auto'>
+          <div className='absolute z-[-2] inset-0 bg-secondary/35 sm:from-sebg-secondary/95 sm:to-sebg-secondary/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l'></div>
+          <JoinUs />
           <div className='container mx-auto py-10'>
-            <div className='grid grid-cols-6 gap-6 py-20'>
-              <div className='col-span-6 md:col-span-3'></div>
-              <div className='col-span-6 md:col-span-3'>
+            <div className='grid grid-cols-6 gap-6 py-0 md:py-20'>
+              <div className='col-span-6 lg:col-span-3'></div>
+              <div className='col-span-6 lg:col-span-3'>
                 <div className='w-full text-white my-auto grid gap-5 p-5'>
-                  <h2 className='text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold'>
+                  <h3 className='text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold'>
                     JOIN <span className='text-primary'>US</span>
-                  </h2>
+                  </h3>
                   <h3 className='my-4 md:my-10 text-xl'>
                     Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
                   </h3>
@@ -183,67 +154,58 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className='mb-56'>
-        <div className='relative'>
-          <Image
-            src={Pic6}
-            className='h-full w-full object-fill absolute z-[-1]'
-            alt={'prb_header'}
-            title={'Prb_Header'}
-            width='0'
-            height='0'
-            quality={100}
-            sizes='100vw'
-          />
-
-          <div className='container mx-auto grid grid-cols-6 gap-36 py-52'>
-            <div className='col-span-6 md:col-span-4'>
-              <div className='grid gap-8 p-4'>
-                <div className='p-6 grid gap-14'>
-                  <h2 className='grid gap-8 text-secondary font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl '>
+      <section>
+        <div className='relative 2xl:container 2xl:mx-auto'>
+          <div className='absolute z-[-2] inset-0 bg-slate-50 sm:from-sebg-secondary/95 sm:to-sebg-secondary/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l'></div>
+          <CallMe />{' '}
+          <div>
+            <div className='w-full lg:w-2/3 py-20 md:py-32 px-2 md:px-20'>
+              <div className='grid gap-8'>
+                <div className='p-6 grid gap-10'>
+                  <h3 className='grid gap-0 text-secondary font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl '>
                     <span>Lorem ipsum</span>{' '}
                     <span className='text-primary'>dolor sit amet</span>
-                  </h2>
-                  <p className='text-lg'>
+                  </h3>
+                  <p className='text-lg text-gray-500 indent-8'>
                     Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
                     diam nonumy eirmod tempor invidunt ut labore et dolore magna
                     aliquyam erat, sed diam voluptua.
                   </p>
 
-                  <button className='uppercase w-max inline-block rounded-md bg-primary px-16 py-6 shadow-custom1 text-center text-sm font-semibold text-white outline-none transition duration-500 ease-in-out transform hover:bg-secondary focus-visible:ring active:bg-secondary md:text-base'>
+                  <button className=' w-max inline-block rounded-md bg-primary px-4 md:px-10 py-2 md:py-4 text-xs md:text-base shadow-custom1 text-center  font-semibold text-white outline-none transition duration-500 ease-in-out transform hover:bg-tertiary focus-visible:ring active:bg-secondary'>
                     VIEW ALL
                   </button>
                 </div>
               </div>
             </div>
-            <div className='col-span-6 md:col-span-2'></div>
           </div>
         </div>
       </section>
       <section>
-        <div className='relative bg-gradient-to-b from-gray-900 to-gray-600 '>
-          <div className='container mx-auto py-20'>
+        <div className=''>
+          <div className='container mx-auto py-20 shadow-custom1 p-10 relative'>
+            <Image
+              src={Pic6White}
+              className='absolute inset-0 h-full w-full object-fill z-[-1]'
+              alt={'prb_header'}
+              title={'Prb_Header'}
+              width='0'
+              height='0'
+              quality={100}
+              sizes='100vw'
+            />
+            <div className='absolute z-[-2] inset-0 bg-slate-50 sm:from-sebg-secondary/95 sm:to-sebg-secondary/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l'></div>
             <div className='flex items-center justify-center'>
               <div className=''>
-                <h2 className='text-white text-center font-bold my-20 text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl'>
+                <h3 className='text-primary text-center font-bold my-20 text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl'>
                   <span>Lorem ipsum </span>{' '}
-                  <span className='text-primary'>dolor sit amet</span>
-                </h2>
-
-                <Carrousel
-                  images={[
-                    '/assets/Pic1.png',
-                    '/assets/Pic2.png',
-                    '/assets/Pic3.png',
-                    '/assets/Pic4.png',
-                    '/assets/Pic5.png',
-                    '/assets/Pic6.png',
-                  ]}
-                />
+                  <span className='text-secondary'>dolor sit amet</span>
+                </h3>
+                <ListImage />
               </div>
             </div>
             <div className='flex w-full justify-center py-10'>
-              <button className='uppercase w-max inline-block rounded-md bg-primary px-16 py-6 shadow-custom1 text-center text-sm font-semibold text-white outline-none transition duration-500 ease-in-out transform hover:bg-secondary focus-visible:ring active:bg-secondary md:text-base'>
+              <button className=' w-max inline-block rounded-md bg-primary px-4 md:px-10 py-2 md:py-4 text-xs md:text-base shadow-custom1 text-center  font-semibold text-white outline-none transition duration-500 ease-in-out transform hover:bg-tertiary focus-visible:ring active:bg-secondary'>
                 VIEW ALL
               </button>
             </div>
