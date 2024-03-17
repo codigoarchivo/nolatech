0ef86790f9cb5a2475399f5e709fb43e5fbb70e3
Jackson Quintero <75138506+codigoarchivo@users.noreply.github.com>
2024-03-16 22:09:17 -0400

    refactor NavSub

--- src/app/ui/NavSub.tsx
+++ src/app/ui/NavSub.tsx
@@ -1,17 +1,19 @@
 'use client';
 
 import Image from 'next/image';
+import Link from 'next/link';
 import { Content } from './Count';
 import { useUIStore } from '@/store';
 import icongrab from '../../../public/assets/icongrab.png';
 import Prb_Header from '../../../public/assets/Prb_Header.png';
+import { navLink } from './nav_link';
 
-export const NavSub = () => {  
+export const NavSub = () => {
   const isSideMenuOpen = useUIStore((state) => state.isSideMenuOpen);
   return (
     <nav>
-      <div className='w-full h-full'>
-        <div className='relative h-screen grid items-center'>
+      <div className='w-full h-full 2xl:container 2xl:mx-auto'>
+        <div className='relative h-[400px] sm:h-[500px] md:h-[600px] lg:h-[760px] grid items-center'>
           <Image
             src={Prb_Header}
             className='h-full w-full object-cover object-top absolute z-[-1]'
@@ -24,7 +26,11 @@ export const NavSub = () => {
           />
           <div className='absolute z-[-1] inset-0 bg-secondary/35 sm:from-sebg-secondary/95 sm:to-sebg-secondary/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l'></div>
 
-          <div className={`${!isSideMenuOpen ? 'flex' : 'hidden'} w-full flex gap-5 justify-around items-center text-white py-10 container mx-auto`}>
+          <div
+            className={`${
+              !isSideMenuOpen ? 'flex' : 'hidden'
+            } w-full flex gap-5 justify-around items-center text-white py-0 container mx-auto`}
+          >
             <Image
               src={icongrab}
               className='h-16 w-48'
@@ -36,39 +42,23 @@ export const NavSub = () => {
               sizes='100vw'
             />
             <div className='flex justify-between gap-5 font-bold  text-sm lg:text-md'>
-              <a href='' className='route'>
-                LOREM IPSUM
-              </a>
-              <a href='' className='route'>
-                LOREM IPSUM
-              </a>
-              <a href='' className='route'>
-                LOREM IPSUM
-              </a>
-              <a href='' className='route'>
-                LOREM IPSUM
-              </a>
-              <a href='' className='route'>
-                LOREM IPSUM
-              </a>
-              <a href='' className='route'>
-                LOREM IPSUM
-              </a>{' '}
+              {navLink.map((item, key) => (
+                <Link href={item.link} key={key} title={item.name + key}>
+                  <span>{item.name}</span>
+                </Link>
+              ))}
             </div>
           </div>
-          <div className='grid grid-cols-6 gap-6 container mx-auto'>
-            <div className='col-span-6 sm:col-span-1 md:col-span-2'></div>
-            <div className='col-span-6 sm:col-span-5 md:col-span-4'>
-              <div className='grid gap-2 items-center w-max text-2xl md:text-4xl lg:text-6xl p-3'>
-                <h2 className='text-white font-bold'>LOREM IPSUM</h2>
-                <h1 className='text-secondary bg-white font-bold max-w-fit p-2 shadow-custom1'>
-                  LOREM IPSUM DOLOR
-                </h1>
-                <h2 className='text-white font-bold mb-14'>LOREM IPSUM</h2>
-                <button className=' w-max inline-block rounded-md bg-primary px-8 py-3 shadow-custom1 text-center text-sm font-semibold text-white outline-none transition duration-500 ease-in-out transform hover:bg-tertiary focus-visible:ring active:bg-secondary md:text-base'>
-                  WHAT IS NEXT
-                </button>
-              </div>
+          <div className='w-full h-full flex justify-center items-center lg:items-start '>
+            <div className='grid gap-2 w-max text-lg sm:text-2xl md:text-4xl lg:text-6xl px-2 md:px-6  ml-0 md:ml-20 lg:ml-40'>
+              <h2 className='text-white font-bold'>LOREM IPSUM</h2>
+              <h1 className='text-secondary bg-white font-bold max-w-fit p-2 shadow-custom1'>
+                LOREM IPSUM DOLOR
+              </h1>
+              <h2 className='text-white font-bold mb-4 md:mb-14'>LOREM IPSUM</h2>
+              <button className=' w-max inline-block rounded-md bg-primary px-4 md:px-10 py-2 md:py-4 text-xs md:text-base shadow-custom1 text-center  font-semibold text-white outline-none transition duration-500 ease-in-out transform hover:bg-tertiary focus-visible:ring active:bg-secondary'>
+                WHAT IS NEXT
+              </button>
             </div>
           </div>
         </div>
