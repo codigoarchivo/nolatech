ea1be16c0c4b921c6bbea9517a4d3a79f403ca9a
Jackson Quintero <75138506+codigoarchivo@users.noreply.github.com>
2024-03-16 22:06:21 -0400

    refactor Content

--- src/app/ui/Count.tsx
+++ src/app/ui/Count.tsx
@@ -1,12 +1,12 @@
 import { Countdown } from './Countdown';
-
+ 
 export const Content = () => {
   const targetDate = new Date('2024-12-31T23:59:59');
   
   return (
-    <div className=' bg-primary text-white grid grid-cols-6 gap-6 px-14 sm:items-center h-40 py-10'>
-      <div className='text-md md:text-lg lg:text-lg xl:text-2xl font-bold col-span-6 md:col-span-2'>
-        <p>Lorem ipsum dolor sit amet, consetetur</p>
+    <div className='bg-primary text-white grid grid-cols-6 gap-6 px-2 py-6 sm:px-12 md:py-14 md:px-14 sm:items-center'>
+      <div className='text-md md:text-lg lg:text-2xl xl:text-3xl font-bold col-span-6 md:col-span-2'>
+        <p>Lorem <span className='text-secondary'>ipsum dolor sit</span> amet, consetetur</p>
       </div>
       <Countdown targetDate={targetDate} /> 
     </div>
