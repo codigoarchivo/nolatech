f180504b1425178929d71e52cf204f7112ad2812
Jackson Quintero <75138506+codigoarchivo@users.noreply.github.com>
2024-07-13 21:18:11 -0400

    add edit delete action

---
+++ src/actions/user/edit_user.ts
@@ -0,0 +1,43 @@
+'use server';
+
+import axiosInstance from '@/lib/axios';
+import { confHeader } from '@/utils/configHeader';
+
+interface Iupdate_user {
+  [k: string]: FormDataEntryValue;
+}
+
+export const update_user = async (register: Iupdate_user) => {
+  try {
+    const formData = {
+      first_name: register.first_name,
+      last_name: register.last_name,
+      email: register.email,
+      is_admin: register.is_admin,
+      profile_image: register.profile_image,
+    };
+
+    
+    const response = await axiosInstance.patch(
+      `${register.base}${register.id}`,
+      formData,
+      confHeader(register.token.toString())
+    );
+
+    if (response.status === 200) {
+      return {
+        ok: true,
+        data: response.data,
+      };
+    } else {
+      return {
+        ok: false,
+      };
+    }
+  } catch (error) {
+    console.log('Error fetching data:', (error as { message: string }).message);
+    return {
+      ok: false,
+    };
+  }
+};
